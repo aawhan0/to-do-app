cb7d040e7a2410d849d2b487bdf077690bfe60d7
Aawhan Vyas <vyasaawhan@gmail.com>
2024-11-12 01:59:05 +0530

    a lot of bs added, m too lazy to write it all ;p

--- app/page.js
+++ app/page.js
@@ -1,30 +1,63 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    setmainTask([...mainTask, {title,desc}]);
+    // console.log(title);
+    // console.log(desc);
+    settitle('')
+    setdesc('')
+    console.log(mainTask);
+    
+  }
+
+
+  let renderTask = <h2> No task available</h2>
   return (
     <>
-    <h1 className='bg-black text-white p-5 text-2xl font-bold text-center'>
+    
+    <h1 className='bg-slate-700 text-white p-5 text-4xl font-bold text-center'>
       My to-do list
     </h1>
     
-    <form>
+    <form onSubmit={submitHandler}>
        <input
          type = "text"
-        className='text-2xl
+        className='text-2xl text-black
        border-zinc-800 border-4 m-8 px-4 py-2'
        placeholder='Enter task here'
+       value={title}
+       onChange={(e)=>{
+       settitle(e.target.value)
+       }}
        />
 
 <input
-         type = "text"
-        className='text-2xl
+        type = "text"
+        className='text-2xl text-black
        border-zinc-800 border-4 m-8 px-4 py-2'
        placeholder='Enter description here'
+       value={desc}
+       onChange={(e)=>{
+       setdesc(e.target.value)
+       }}
        />
       
-      <button className='bg-black text-white text-2xl px-4 py-3 font-bold rounded-lg'> Add task</button>
+      <button className='bg-white text-black text-2xl px-4 py-3 m-4 font-bold rounded-lg'> Add task</button>
 
     </form>
+    <hr />
+    <div className='p-8 bg-slate-200'>
+      <ul className='text-black text-2xl  font-bold text-center'>
+        {renderTask}
+      </ul>
+    </div>
     </>
   )
 }
